0481a5b775b6fde83db714f522edb916e0ff4549
lepfau <edouard.pfauwadel@gmail.com>
2021-03-04 16:37:52 +0100

    refactored with hooks

--- src/views/ListBeers.jsx
+++ src/views/ListBeers.jsx
@@ -1,44 +1,83 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navmain from '../components/NavMain';
 import axios from 'axios';
 import { NavLink } from 'react-router-dom';
-export default class ListBeers extends Component {
-  state = {
-    beers: [],
-  };
 
-  componentDidMount() {
+function ListBeers() {
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
     axios
       .get('https://ih-beers-api2.herokuapp.com/beers')
-      .then((responseFromApi) => {
-        console.log(responseFromApi.data);
-        this.setState({
-          beers: responseFromApi.data,
-        });
+      .then((respFromApi) => {
+        setBeers(respFromApi.data);
+      })
+      .catch((err) => {
+        console.log(err);
       });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <Navmain />
-        <h1>ALL BEERS</h1>
-        {this.state.beers.map((beer) => {
-          return (
-            <div key={beer._id} style={{ border: 'solid 1px black' }}>
-              <h3>{beer.name}</h3>
-              <p>{beer.description}</p>
-              <img
-                src={beer.image_url}
-                style={{ width: '100px', height: 'auto' }}
-              />
-              <NavLink exact to={`/beers/${beer._id}`}>
-                Details...
-              </NavLink>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Navmain />
+      <h1>ALL BEERS</h1>
+      {beers.map((beer) => {
+        return (
+          <div key={beer._id}>
+            <h3>{beer.name}</h3>
+            <img
+              src={beer.image_url}
+              style={{ width: '100px', height: 'auto' }}
+            />
+            <NavLink exact to={`/beers/${beer._id}`}>
+              Details...
+            </NavLink>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
+
+export default ListBeers;
+
+// export default class ListBeers extends Component {
+//   state = {
+//     beers: [],
+//   };
+
+//   componentDidMount() {
+//     axios
+//       .get('https://ih-beers-api2.herokuapp.com/beers')
+//       .then((responseFromApi) => {
+//         console.log(responseFromApi.data);
+//         this.setState({
+//           beers: responseFromApi.data,
+//         });
+//       });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <Navmain />
+//         <h1>ALL BEERS</h1>
+//         {this.state.beers.map((beer) => {
+//           return (
+//             <div key={beer._id} style={{ border: 'solid 1px black' }}>
+//               <h3>{beer.name}</h3>
+//               <p>{beer.description}</p>
+//               <img
+//                 src={beer.image_url}
+//                 style={{ width: '100px', height: 'auto' }}
+//               />
+//               <NavLink exact to={`/beers/${beer._id}`}>
+//                 Details...
+//               </NavLink>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     );
+//   }
+// }
--- src/views/SingleBeer.jsx
+++ src/views/SingleBeer.jsx
@@ -1,46 +1,83 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
 import Navmain from "../components/NavMain"
 import axios from "axios";
 import { NavLink } from 'react-router-dom';
 
-export default class SingleBeer extends Component {
 
-    state = {
-        beer: null,
-      };
 
-      componentDidMount() {
-        // console.log(this.props.match.params.id);
-        const beerId = this.props.match.params.id;
-    
-        axios.get("https://ih-beers-api2.herokuapp.com/beers/" + beerId).then((apiResponse) => {
-          console.log(apiResponse);
-          
-            this.setState({
-              beer: apiResponse.data,
-            });
-         
-        });
-      }
+function SingleBeer(props) {
 
-      render() {
-        if (!this.state.beer) {
-          return <div>Loading.....</div>;
-        }
-    
+  const [beer, setBeer] = useState([])
+
+useEffect(() => {
+  const beerId = props.match.params.id
+  axios.get("https://ih-beers-api2.herokuapp.com/beers/" + beerId )
+  .then((respFromApi) => {
+    console.log(respFromApi.data)
+    setBeer([respFromApi.data])
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+}, [])
+
+  return (
+    <div>
+      {beer.map((beer) => {
         return (
           <div>
-              <Navmain />
-              <NavLink exact to={`/listbeers`}>
-               Back to beers list
-              </NavLink>
-            <h2> Beer page</h2>
-            <div>
-              <h2>{this.state.beer.name}</h2>
-              <img src={this.state.beer.image_url} />
+            <p>{beer.name}</p>
+            <img src={beer.image_url}/>
+        <p>{beer.description}</p>
             </div>
-            {/* <pre>{JSON.stringify(this.props, null, 2)}</pre> */}
-          </div>
-        );
-      }
+        )
+      })}
+    </div>
+  )
 }
+
+export default SingleBeer
+
+
+// export default class SingleBeer extends Component {
+
+//     state = {
+//         beer: null,
+//       };
+
+//       componentDidMount() {
+//         // console.log(this.props.match.params.id);
+//         const beerId = this.props.match.params.id;
+    
+//         axios.get("https://ih-beers-api2.herokuapp.com/beers/" + beerId).then((apiResponse) => {
+//           console.log(apiResponse);
+          
+//             this.setState({
+//               beer: apiResponse.data,
+//             });
+         
+//         });
+//       }
+
+//       render() {
+//         if (!this.state.beer) {
+//           return <div>Loading.....</div>;
+//         }
+    
+//         return (
+//           <div>
+//               <Navmain />
+//               <NavLink exact to={`/listbeers`}>
+//                Back to beers list
+//               </NavLink>
+//             <h2> Beer page</h2>
+//             <div>
+//               <h2>{this.state.beer.name}</h2>
+//               <img src={this.state.beer.image_url} />
+//             </div>
+//             {/* <pre>{JSON.stringify(this.props, null, 2)}</pre> */}
+//           </div>
+//         );
+//       }
+// }
